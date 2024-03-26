79b6e9f8ee3959bfe3a562aafb43fef9fbf71ac5
szjunior <kR40n8vz@git>
2024-03-26 15:23:47 -0300

    add total sentence, company etc...

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1, align="R")
         pdf.cell(w=30, h=8, txt=str(row["total_price"]),border=1, ln=1, align="R")
 
+    # add total row
     total_sum = df["total_price"].sum()
     pdf.set_font(family="Times", size=10)
     pdf.set_text_color(80, 80, 80)
@@ -50,6 +51,16 @@ for filepath in filepaths:
     pdf.cell(w=70, h=8, txt="", border=1)
     pdf.cell(w=35, h=8, txt="", border=1, align="C")
     pdf.cell(w=30, h=8, txt="", border=1, align="R")
-    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1, align="R")
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, align="R", ln=1)
+
+    # add total sum sentence
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum} euros !", ln=1)
+
+    # add company name and logo
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt=f"SCJ Python DevOps")
+
+
 
     pdf.output(f"PDFS/{nr_invoice}.pdf")
